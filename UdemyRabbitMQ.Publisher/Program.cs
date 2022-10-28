2b2eec28154f056f7eb7119461503c1ff10ea792
Murat Çiçek <murat1347@yandex.com>
2022-10-28 18:12:22 +0300

    Add project files.

---
+++ UdemyRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://srkkvjpy:6rvSFvPpimqp1NSStne5UFFVDG7dzhr9@sparrow.rmq.cloudamqp.com/srkkvjpy");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+channel.QueueDeclare("Hello queue",true,false,false);
+
+string message = "hello world";
+
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty,"Hello queue",null,messageBody);
+Console.WriteLine("Mesajınız gönderilmiştir!");
+Console.ReadLine();
---
+++ UdemyRabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://srkkvjpy:6rvSFvPpimqp1NSStne5UFFVDG7dzhr9@sparrow.rmq.cloudamqp.com/srkkvjpy");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+//channel.QueueDeclare("Hello queue", true, false, false);
+
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume("Hello queue",true,consumer);
+
+
+consumer.Received += Consumer_Received;
+
+void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine("Gelen Mesaj : "+ message);
+}
+
+Console.ReadLine();
